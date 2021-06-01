8bc86df3fda4b4d3591c50fab69061267189e348
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-01 18:11:00 +0530

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,8 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Congratulations, your extension "vs" is now active!');
 	let disposable = vscode.commands.registerCommand("vsint.helloworld", () => {
-		vscode.window.showInformationMessage('Hello World from Vsinterest!');
-		HelloWorldPanel.createOrShow(context.extensionUri)
+	vscode.window.showInformationMessage('Hello World from Vsinterest!');
+	HelloWorldPanel.createOrShow(context.extensionUri)
 	});
 
 	context.subscriptions.push(disposable);
